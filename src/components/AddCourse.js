fc1182e865e6162e9e39d4a3b7310f6d261ed438
Tyler Lima <tpl8254@rit.edu>
2018-11-27 15:11:27 -0500

    add Course page change
    Selelcting course section is now a dropdown

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 import NavBar from '../containers/NavBar'
 
-const options = [
+const g_size_options = [
   { key: 'NA', text: 'Any Size', value: 'NA'},
   { key: '1', text: '1', value: '1' },
   { key: '2', text: '2', value: '2' },
@@ -22,6 +22,25 @@ const options = [
   { key: '14', text: '14', value: '14' },
   { key: '15', text: '15', value: '15' },
 ]
+
+const course_select_options = [
+  { key: '01', text: '01', value: '01' },
+  { key: '02', text: '02', value: '02' },
+  { key: '03', text: '03', value: '03' },
+  { key: '04', text: '04', value: '04' },
+  { key: '05', text: '05', value: '05' },
+  { key: '06', text: '06', value: '06' },
+  { key: '07', text: '07', value: '07' },
+  { key: '08', text: '08', value: '08' },
+  { key: '09', text: '09', value: '09' },
+  { key: '10', text: '10', value: '10' },
+  { key: '11', text: '11', value: '11' },
+  { key: '12', text: '12', value: '12' },
+  { key: '13', text: '13', value: '13' },
+  { key: '14', text: '14', value: '14' },
+  { key: '15', text: '15', value: '15' },
+]
+
 
 
 export default class AddCourse extends Component{
@@ -82,9 +101,9 @@ export default class AddCourse extends Component{
             <Form>
               <Form.Group widths='4'>
                 <Form.Input fluid label='Course ID' placeholder='(eg: SWEN-101)' onChange={this.onCourseIdChange} error={this.state.course_id_error}/>
-                <Form.Input fluid label='Course Section' placeholder='(eg: 05)' onChange={this.onCourseSectionChange} error={this.state.course_sect_error} />
+                <Form.Select width='2' fluid label='Course Section' options={course_select_options} placeholder='(eg: 05)' onChange={this.onCourseSectionChange} error={this.state.course_sect_error} />
               </Form.Group>
-              <Form.Select width='2' fluid label='Ideal Group size' options={options} placeholder='(eg: 4)' onChange={this.onClassNumChange} error={this.state.class_num_error}/>
+              <Form.Select width='2' fluid label='Ideal Group size' options={g_size_options} placeholder='(eg: 4)' onChange={this.onClassNumChange} error={this.state.class_num_error}/>
 
               <Form.Checkbox label='NTID student' />
               <Modal trigger={<Form.Button onClick={this.onSubmit}>Submit</Form.Button>}
